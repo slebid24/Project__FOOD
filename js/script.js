884b6d727a53f6f36d7b066b6bc2640f32208a30
Serhii Lebid <lebids24@gmail.com>
2022-05-01 15:47:00 +0300

    timer done

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 window.addEventListener("DOMContentLoaded", () => {
+   // Табы
+
    const tabs = document.querySelectorAll(".tabheader__item");
    const tabsContent = document.querySelectorAll(".tabcontent");
    const tabsParent = document.querySelector(".tabheader__items");
@@ -31,7 +33,7 @@ window.addEventListener("DOMContentLoaded", () => {
    showTabContent();
 
    tabsParent.addEventListener("click", (e) => {
-      const target = e.target; 
+      const target = e.target;
       // можно создать переменную и поместить в неё е.таргет
       if (target && target.classList.contains("tabheader__item")) {
          tabs.forEach((item, i) => {
@@ -39,17 +41,85 @@ window.addEventListener("DOMContentLoaded", () => {
                hideTabContent();
                showTabContent(i);
             }
-         }
-      );
+         });
+      }
+   });
+   // Создали делегирование событий, после условия создали перебор псевдомасива с табами, 
+   // если айтем псевдомасива совпадает с таргетом, то выводим две функции
+   // одна закрывает все табы, вторая показывает таб с тем номером, на который кликнул пользователь 
+   // (айтем проверяется условием, и по условию присваивается номер, который передаётся в функцию)
+
+
+   // Таймер
+
+   const deadline = "2022-05-09";
+   // Дедлайн таймера
+
+
+   function getTimeRemaining(endtime) {
+      // функция, вычесляющая данные для таймера
+      const t = Date.parse(endtime) - Date.parse(new Date());
+      const days = Math.floor(t / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+      const minutes = Math.floor((t / 1000 / 60 ) % 60);
+      const seconds = Math.floor((t / 1000) % 60);
+      // В переменной Т = разница между дедлайном и текущим временем
+      // Все следующие переменные вычесляют дни, часы.... в остатке + с 
+      // округлнеие Math.floor(округляет к меньшему)
+      return {
+         "total" : t,
+         "days" : days,
+         "hours" : hours,
+         "minutes" : minutes,
+         "seconds" : seconds
+      };
+      // функция возвращает обьект с данными
    }
-});
-// Создали делегирование событий, после условия создали перебор псевдомасива с табами, 
-// если айтем псевдомасива совпадает с таргетом, то выводим две функции
-// одна закрывает все табы, вторая показывает таб с тем номером, на который кликнул пользователь 
-// (айтем проверяется условием, и по условию присваивается номер, который передаётся в функцию)
 
+   function getZero(num) {
+      if (num > 0 && num < 10) {
+         return `0${num}`;
+      } else {
+         return num;
+      }
+   }
+   // функция, которая подставляем 0, когда значения в таймере не двузначное
 
+   function setClock(selector, endtime) { 
+      // функция таймер, в параметрах селектор елемента на стр (таймеров может быть
+      // много), второй - наш дедлайн
+      const timer = document.querySelector(selector);
+      const days = timer.querySelector("#days");
+      const hours = timer.querySelector("#hours");
+      const minutes = timer.querySelector("#minutes");
+      const seconds = timer.querySelector("#seconds");
+      const timeInterval = setInterval(updateClock, 1000);
+      // создаём интревальный запуск функции, которая обновляет данные
+      // на странице (раз в 1 секунду)
 
+      updateClock();
+      // вызывает функцию на страницу сразу, не дожидаясь задержки в 1000мс
+
+      function updateClock() {
+         const t = getTimeRemaining(endtime);
+         // помещаем в переменную обьект из 1 функции
+
+         days.innerHTML = getZero(t.days);
+         hours.innerHTML = getZero(t.hours);
+         minutes.innerHTML = getZero(t.minutes);
+         seconds.innerHTML = getZero(t.seconds);
+         // присваиваем елементам на странице соответсвующии значения из функции
+
+         if(t.total <= 0) {
+            clearInterval(timeInterval);
+         }
+         // останавливаем функцию в случае, если разница в времени будет 0
+      }
+   }
+
+   setClock(".timer", deadline);
+
+   
 
 
 });
